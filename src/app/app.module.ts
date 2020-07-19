9c79647618d89279604be5a6cb38fc79cf258cdd
Nouvan <nouvan.sodak@gmail.com>
2020-07-19 11:51:56 +0200

    Add dependencies and install packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FiltersComponentComponent } from './filters-component/filters-component.component';
+import { DonutComponentComponent } from './donut-component/donut-component.component';
+import { TableComponentComponent } from './table-component/table-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponentComponent,
+    DonutComponentComponent,
+    TableComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
